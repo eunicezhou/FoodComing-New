99af23338bd02d00d4cf2f41248a03e294a2dbf2
eunicechou <zizhen0826@gmail.com>
2025-10-22 11:46:42 +0800

    feat: create test command

--- foodcoming.py
+++ foodcoming.py
@@ -1,10 +1,14 @@
+import unittest
 from flask_migrate import Migrate
 import os
 
 from app import create_app, db
-from config import config
-from app.models.models import Store, Menu, Member
-
 
 app = create_app(os.environ.get('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+@app.cli.command(name="test")
+def test():
+    tests = unittest.TestLoader().discover("tests")
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(tests)
--- invoke_test.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-if __name__ == "__main__":
-    tests = unittest.TestLoader().discover("tests")
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(tests)
